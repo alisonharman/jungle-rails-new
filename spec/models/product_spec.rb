287f822703342db969f3cb2c3d9bbdcfd11ac76c
Alison Harman <alison_harman@sympatico.ca>
2022-09-27 21:14:16 +0000

    fix formatting

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -13,7 +13,6 @@ RSpec.describe Product, type: :model do
     end
 
     it 'fails with no name' do
-
       category = Category.create(name: 'Apparel')
       product = category.products.new
       product.update(price_cents: 100, quantity: 10, id: 1, description: "hello", image: "www.no.go")
@@ -26,15 +25,20 @@ RSpec.describe Product, type: :model do
       expect(product.errors.full_messages).to include("Price can't be blank")
     end
 
+    it 'fails with no price' do
+      product = Product.create(quantity: 10, id: 1, description: "hello", image: "www.no.go")
+      expect(product.errors.full_messages).to include("Price can't be blank")
+    end
+  
     it 'fails with no quantity' do
       product = Product.create(price_cents: 100, name: "name", id: 1, description: "hello", image: "www.no.go")
       expect(product.errors.full_messages).to include("Quantity can't be blank")
     end
-
+  
     it 'fails with no category' do
       product = Product.create(price: 100, quantity: 10, id: 1, description: "hello", image: "www.no.go")
       expect(product.errors.full_messages).to include("Category can't be blank")
     end
-  
+    
   end
 end
