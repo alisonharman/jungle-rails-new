75d9ef7f6e25aa2209b1dde79dcfe07f3a404758
Alison Harman <alison_harman@sympatico.ca>
2022-10-05 01:08:06 +0000

    add test to validate that password is rejected with less than 6 chars

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,11 +29,11 @@ RSpec.describe User, type: :model do
 
     it "should reject if the password_confirmation and password don't match" do
       @user = User.new({
-        first_name: 'Walter',
-        last_name: 'White',
-        email: 'ww@qmail.com',
-        password: 'ww0000',
-        password_confirmation: '0000'
+        first_name: 'Amu',
+        last_name: 'Jones',
+        email: 'amy.123456com',
+        password: '123456',
+        password_confirmation: 'unique'
       })
       save = @user.save
       errors = @user.errors.full_messages
@@ -44,10 +44,10 @@ RSpec.describe User, type: :model do
 
     it 'should not accept blank email' do
       @user = User.new({
-        first_name: 'Walter',
-        last_name: 'White',
-        password: 'ww0000',
-        password_confirmation: 'ww0000'
+        first_name: 'Amy',
+        last_name: 'Jones',
+        password: '123456',
+        password_confirmation: '123456'
         })
       @user.save
       errors = @user.errors.full_messages
@@ -57,9 +57,9 @@ RSpec.describe User, type: :model do
     it 'should have a first_name' do
       @user = User.new({
         last_name: 'White',
-        email: 'ww@qmail.com',
-        password: 'ww0000',
-        password_confirmation: 'ww0000'
+        email: 'ww@unique.com',
+        password: 'unique',
+        password_confirmation: 'unique'
       })
       @user.save          
       errors = @user.errors.full_messages
@@ -68,14 +68,28 @@ RSpec.describe User, type: :model do
     it 'should have a last_name' do
       @user = User.new({
         first_name: 'Walter',
-        email: 'ww@qmail.com',
-        password: 'ww0000',
-        password_confirmation: 'ww0000'
+        email: 'walter@qmail.com',
+        password: 'ww12345',
+        password_confirmation: 'ww12345'
       })
       @user.save          
       errors = @user.errors.full_messages
       expect(errors).to include("Last name can't be blank")
     end
  
+    it 'should reject if the password has less than 6' do
+      @user = User.new({
+        first_name: 'Davy',
+        last_name: 'Jones',
+        email: 'jones@gmail.com',
+        password: 'jones',
+        password_confirmation: 'jones'
+      })
+      save = @user.save
+      errors = @user.errors.full_messages
+
+      expect(save).to be false
+      expect(errors).to include("Password is too short (minimum is 6 characters)")
+    end
   end
 end
