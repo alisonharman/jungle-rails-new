86d1a71d62722c30e7b1665fba2ab41742fe41f0
Alison Harman <alison_harman@sympatico.ca>
2022-10-05 00:51:57 +0000

    add validation that password and password confirmation  need to match

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,31 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
-  it 'should save a user when all fields are filled out' do
-    @user = User.new({
-      first_name: 'Amy',
-      last_name: 'Jones',
-      email: 'amy@1234.com',
-      password: '1234',
-      password_confirmation: '1234' 
-    })
-    expect(@user.save).to eql(true)
-  end
-
-  it 'should reject if the password field is empty' do
-    @user = User.new({
-      first_name: 'Amy',
-      last_name: 'Jones',
-      email: 'amy@1234.com',
-      password_confirmation: '1234' 
-    })
-    expect(@user.save).to eql(false)
-    expect(@user.errors.full_messages).to include("Password can't be blank")
-  end
-
-
   describe 'Validations' do
-    # validation examples here
+
+  
+    it 'should save a user when all fields are filled out' do
+      @user = User.new({
+        first_name: 'Amy',
+        last_name: 'Jones',
+        email: 'amy@1234.com',
+        password: '1234',
+        password_confirmation: '1234' 
+      })
+      expect(@user.save).to eql(true)
+    end
+
+    it 'should reject if the password field is empty' do
+      @user = User.new({
+        first_name: 'Amy',
+        last_name: 'Jones',
+        email: 'amy@1234.com',
+        password_confirmation: '1234' 
+      })
+      expect(@user.save).to eql(false)
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+
+
+    it "should reject if the password_confirmation and password don't match" do
+      @user = User.new({
+        first_name: 'Walter',
+        last_name: 'White',
+        email: 'ww@qmail.com',
+        password: 'ww0000',
+        password_confirmation: '0000'
+      })
+      save = @user.save
+      errors = @user.errors.full_messages
+
+      expect(save).to be false
+      expect(errors).to include("Password confirmation doesn't match Password")
+    end
+ 
   end
 end
