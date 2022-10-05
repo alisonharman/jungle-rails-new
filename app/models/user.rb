7782c105e2e1d3b65e2dba8e773c8ce793d770b0
Alison Harman <alison_harman@sympatico.ca>
2022-10-05 00:59:48 +0000

    add validation to fields of last_name, first_name, password, etc

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
 
   has_secure_password
+  validates_presence_of :password_confirmation
+    validates :password, length: { in: 6..72 }
+    validates_presence_of :first_name
+    validates_presence_of :last_name
+    validates_presence_of :email
 
 end
