d6765da5565f8404de1b1dc1879df41140fa6bc3
Alison Harman <alison_harman@sympatico.ca>
2022-10-12 22:11:52 +0000

    add test to determine that all products are loaded onto home page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -10,4 +10,9 @@ describe('Jungle Rails app', () => {
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
   })
+
+  it("There are 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  })
+  
 })
